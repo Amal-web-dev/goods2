d1cea7818d5f6cd38cecc96bbf6bd35895c67ca4
Amal <amalabdurahmonov427@gmail.com>
2023-05-09 11:56:48 +0300

    third

--- index.js
+++ index.js
@@ -1,175 +1,7 @@
-// import products from './modules/db.js'
-
-// let cont = document.querySelector('.container')
-// let data_showFive = document.querySelector('button[data-showFive]')
-// let data_showAll = document.querySelector('button[data-showAll]')
-// let shopped = document.querySelector('.shopped')
-// let bg = document.querySelector('.bg')
-// let data_cart = document.querySelector('button[data-cart]')
-// let cart_amount = document.querySelector('#cart_amount')
-// let howProduct = document.querySelector('.how-product')
-
-
-// let icons = ['price', 'rate', 'count']
-// let cart = []
-
-// reload(products, cont)
-
-// function reload(arr, place) {
-//     place.innerHTML = ""
-//     cart_amount.innerHTML = cart.length
-//     howProduct.innerHTML = cart.length
-    
-//     for (let item of arr) {
-//         let item_div = document.createElement('div')
-//         let img_content = document.createElement('img')
-//         let decr_div = document.createElement('div')
-//         let h3 = document.createElement('h3')
-//         let p = document.createElement('p')
-//         let row = document.createElement('div')
-//         let btn = document.createElement('button')
-    
-//         for (let icon of icons) {
-//             let feature = document.createElement('div')
-//             let img = document.createElement('img')
-//             let span = document.createElement('span')
-    
-//             feature.classList.add('feature')
-    
-//             img.src = "./public/icons/" + icon + ".svg"
-//             span.innerHTML = typeof (item[icon]) !== 'undefined' ? item[icon] : item.rating[icon]
-    
-//             feature.append(img, span)
-//             row.append(feature)
-//         }
-    
-//         item_div.classList.add('item')
-//         img_content.classList.add('content')
-//         decr_div.classList.add('description')
-//         row.classList.add('row')
-    
-//         h3.innerHTML = item.category
-//         p.innerHTML = item.description.slice(0, 80).toLowerCase()
-//         btn.innerHTML = "В избранное"
-    
-//         img_content.src = item.image
-    
-    
-//         item_div.append(img_content, decr_div)
-//         decr_div.append(h3, p, row, btn)
-//         place.append(item_div)
-
-
-
-//         let counter = document.querySelectorAll('.counter')
-      
-
-//         
-
-//         let product = []
-        // let imgProduct = document.querySelectorAll('.img-product')
-        // let pName = document.querySelectorAll('.p-name')
-        // let pRate = document.querySelectorAll('.p-rate')
-        // let pBox = document.querySelectorAll('.p-box')
-        // let pRateTwo = document.querySelectorAll('.p-rate-2')
-        // let pBoxTwo = document.querySelectorAll('.p-box-2')
-//         let totalSum = document.querySelector('.total-sum')
-//         let checkboxContainer = document.querySelectorAll('.checkbox-container')
-//         const totalSumProduct = document.querySelectorAll('.total-sum-product');
-//         let totalProduct = 0;
-        
-//         // item.forEach(item => {
-//         //   const price = item.price;
-//         //   const total = price * quantity;
-//         //   totalSum += total;
-//         // });
-        
-        
-
-
-//         btn.onclick = () => {
-
-
-
-//               if(cart.includes(item.id)) {
-//                 cart = cart.filter(id => id !== item.id)
-//                 btn.classList.remove('add-to-mark')
-//                 btn.innerHTML = "B избранное"
-//                 imgProduct.forEach(img => {
-//                     img.src = ''
-//                     pName.forEach(name => {
-//                         name.innerHTML = ''
-//                         name.classList.remove('block')
-//                     })
-//                     pRate.forEach(rate => {
-//                         rate.innerHTML = ''
-//                     })
-//                     pBox.forEach(box => {
-//                         box.innerHTML = ''
-//                     })
-//                     pRateTwo.forEach(rate => {
-//                         rate.classList.remove('block')
-//                     })
-//                     pBoxTwo.forEach(box => {
-//                         box.classList.remove('block')
-//                     })
-//                     checkboxContainer.forEach(boxCont => {
-//                         boxCont.classList.remove('block')
-//                     })
-//                     counter.forEach(cnt => {
-//                         cnt.classList.remove('block')
-//                     })
-//                     totalSum.innerHTML = '0'
-//                     totalSumProduct.forEach(product => {
-//                         product.innerHTML = ''
-//                       });
-                    
-//                 })
-//             } else {
-//                 cart.push(item.id)
-//                 btn.classList.add('add-to-mark')
-//                 btn.innerHTML = "Добавлено"
-//                 product.push(item)
-                    
-                 
-//             }
-
-//             cart_amount.innerHTML = cart.length
-//             howProduct.innerHTML = cart.length
-//             console.log(cart);
-//         }
-
-//     }
-
-// }
-
-
-// data_showFive.onclick = () => {
-//     reload(products.slice(0,5), cont)
-// }
-// data_showAll.onclick = () => {
-//     reload(products, cont)
-// }
-
-// data_cart.onclick = () => {
-//     shopped.style.right = '0'
-//     bg.style.left = '0'
-//     bg.style.scale = '1'
-
-// }
-// bg.onclick = () => {
-//     shopped.style.right = '-33%'
-//     bg.style.scale = '0'
-//     bg.style.left = '-100%'
-// }
-
-
-
-
-
 import products from './modules/db.js'
 
 let cont = document.querySelector('.container')
+
 let data_showFive = document.querySelector('button[data-showFive]')
 let data_showAll = document.querySelector('button[data-showAll]')
 let shopped = document.querySelector('.shopped')
@@ -247,13 +79,16 @@ function reload(arr, place) {
             }
 
             cart_amount.innerHTML = cart.length
-            console.log(cart);
+            // console.log(cart);
             cart_reload()
         }
 
     }
 
 }
+
+let selected = []
+let selectedItems = document.querySelectorAll('.selected-item');
 
 function cart_reload() {
     cart_place.innerHTML = ''
@@ -272,27 +107,15 @@ function cart_reload() {
     }
 
 
-    // const decrementBtn = document.querySelectorAll('.decrement');
-//         const incrementBtn = document.querySelectorAll('.increment');
-//         const counterValue = document.querySelectorAll('.value');
-//         let count = 1;
-        
-//         counter.forEach((counter, index) => {
-        
-//           decrementBtn[index].addEventListener('click', () => {
-//             if (count > 1) {
-//               count--;
-//               counterValue[index].textContent = count;
-//             }
-//           });
-        
-//           incrementBtn[index].addEventListener('click', () => {
-//             count++;
-//             counterValue[index].textContent = count;
-//           });
-//         });
 
 let count = 1;
+let modalDel = document.querySelector('.modal-delete')
+let modalNo = document.querySelector('.btn-no')
+let modalYes = document.querySelector('.btn-yes')
+let btnReg = document.querySelector('#btn-modal')
+let modalReg = document.querySelector('.modal-reg')
+let btnAccept = document.querySelector('#btn-accept')
+let inputs = document.querySelectorAll('.inputs')
 
 
     for (let item of temp) {
@@ -313,9 +136,6 @@ let count = 1;
         let imgTrush = document.createElement('img')
         let itemPrice = document.createElement('p')
 
-
-
-
         selectedItem.classList.add('selected-item')
         btn.classList.add('del-style')
         checkbox.classList.add('checkbox-style')
@@ -328,7 +148,6 @@ let count = 1;
         itemPrice.classList.add('item-price')
         imgItem.src = item.image
         delete item.image
-        console.log(selectedItem);
         checkbox.type = "checkbox"
         checkbox.checked = true
         pName.innerHTML = item.title.slice(0, 20)
@@ -340,6 +159,8 @@ let count = 1;
         imgTrush.src =  './public/icons/trush.png'
         itemPrice.innerHTML = (item.price * count).toLocaleString('ru-RU') + '$' 
         btn.innerHTML = "Delete"
+
+        selected.push(selectedItem);
 
         selectedItem.prepend(checkbox, leftBlock, centerBlock, rightBlock)
         leftBlock.append(imgItem)
@@ -353,6 +174,7 @@ let count = 1;
         all_prices += item.price * item.qt
 
 
+
         checkbox.onchange = () => {
             if (checkbox.checked) {
                 all_prices += item.price * item.qt
@@ -363,14 +185,44 @@ let count = 1;
         }
 
         btn.onclick = () => {
-            let sure = confirm('Are you sure')
-            if (sure) {
+            modalDel.style.display = 'block'
+            
+            modalYes.onclick = () => {
                 cart = cart.filter(id => id !== item.id)
                 cart_reload()
                 reload(products, cont)
+                modalDel.style.display = 'none'
+            }
+
+            modalNo.onclick = () => {
+                modalDel.style.display = 'none'
             }
         }
 
+        selectedItems.forEach(item => {
+            let checkbox = item.querySelector('input[type="checkbox"]');
+            if (checkbox.checked) {
+              
+            }
+          });
+
+          btnReg.onclick = () => {
+            modalReg.style.display = 'block'
+          }
+
+          btnAccept.onclick = () => {
+            inputs.forEach(inp => {
+                if(inp.value >= 4) {
+                    modalReg.style.display = 'none'
+                    cart = cart.filter(id => id !== item.id)
+                    cart_reload()
+                    reload(products, cont)
+                    } else {
+                            inp.style.border = '2px solid red'
+                    }
+            })
+            
+          }
 
     decrementBtn.addEventListener('click', () => {
         if (count >= 2) {
@@ -396,7 +248,28 @@ let count = 1;
     }
 }
 
+const form = document.forms.sign_up
 
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    selected.splice(0, 1)
+    let user = {
+        customer: {},
+        selected
+    }
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        user.customer[key] = value
+    })
+
+    form.reset()
+
+    console.log(user);
+}
 
 data_showFive.onclick = () => {
     reload(products.slice(0, 5), cont)
@@ -429,3 +302,4 @@ bg.onclick = () => {
 //         {}
 //     ]
 // }
+
